69d69aded061d82e0b0d07b412a575b3f40b1623
Kristoffer <kristoffer.mysen@gmail.com>
2016-02-10 23:13:43 +0100

    Lagt til location services samt POST av location til hardkodet server url

--- Android/MyFriends/app/src/main/java/com/bouvet/sandvika/myfriends/MapsActivity.java
+++ Android/MyFriends/app/src/main/java/com/bouvet/sandvika/myfriends/MapsActivity.java
@@ -1,8 +1,22 @@
 package com.bouvet.sandvika.myfriends;
 
-import android.support.v4.app.FragmentActivity;
+import android.location.Location;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
 
+import com.android.volley.Request.Method;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.LocationListener;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
@@ -10,20 +24,79 @@ import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
-public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
+import org.json.JSONObject;
 
-    private GoogleMap mMap;
+public class MapsActivity extends AppCompatActivity implements LocationListener, OnMapReadyCallback, GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener {
+
+    private final static String REGISTER_USER_URL = "http://192.168.1.124:8080/user";
+    private final static String REGISTER_LOCATION_URL = "http://192.168.1.124:8080/user/kris/updatePosition";
+
+    private GoogleApiClient googleApiClient;
+    private RequestQueue requestQueue;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_maps);
+
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
+
+        googleApiClient = new GoogleApiClient.Builder(this)
+                .addConnectionCallbacks(this)
+                .addOnConnectionFailedListener(this)
+                .addApi(LocationServices.API)
+                .build();
+
+        requestQueue = Volley.newRequestQueue(this);
+
+        createUser();
+
     }
 
+    private void createUser() {
+
+        JsonObjectRequest createUserRequest = new JsonObjectRequest(Method.POST, REGISTER_USER_URL, getUserAsJSON(), new Response.Listener<JSONObject>() {
+            @Override
+            public void onResponse(JSONObject response) {
+                System.out.println("ok");
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                System.out.println("ok");
+            }
+        });
+
+        requestQueue.add(createUserRequest);
+
+    }
+
+    private String getUserAsJSON() {
+
+        return "{\n" +
+                "\"userName\":\"kris\",\n" +
+                "\"firstName\":\"kristoffer\",\n" +
+                "\"lastName\":\"mysen\",\n" +
+                "\"regId\":\"9999\"\n" +
+                "}";
+
+    }
+
+
+    @Override
+    protected void onStart() {
+        googleApiClient.connect();
+        super.onStart();
+    }
+
+    @Override
+    protected void onStop() {
+        googleApiClient.disconnect();
+        super.onStop();
+    }
 
     /**
      * Manipulates the map once available.
@@ -36,11 +109,60 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
      */
     @Override
     public void onMapReady(GoogleMap googleMap) {
-        mMap = googleMap;
 
         // Add a marker in Sydney and move the camera
         LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        googleMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
+        googleMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+    }
+
+    @Override
+    public void onConnected(@Nullable Bundle bundle) {
+        LocationRequest locationRequest = new LocationRequest();
+        locationRequest.setInterval(5000);
+        locationRequest.setFastestInterval(5000);
+        locationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+
+        try {
+            LocationServices.FusedLocationApi.requestLocationUpdates(googleApiClient, locationRequest, this);
+        } catch (SecurityException e) {
+            System.out.println("hmm");
+        }
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+
+    }
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+
+    }
+
+    @Override
+    public void onLocationChanged(final Location location) {
+        JsonObjectRequest postLocationRequest = new JsonObjectRequest(Method.POST, REGISTER_LOCATION_URL, getLocationAsJSON(location), new Response.Listener<JSONObject>() {
+            @Override
+            public void onResponse(JSONObject response) {
+                System.out.println("something");
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                System.out.println("something else");
+            }
+        });
+
+        requestQueue.add(postLocationRequest);
+
+    }
+
+    private String getLocationAsJSON(Location location) {
+        // Her burde man bruke et JSON bibliotek!
+        return "{\n" +
+                "\"location\":\"" + location.getLatitude() + "," + location.getLongitude() + "\"\n" +
+                "}";
+
     }
 }
